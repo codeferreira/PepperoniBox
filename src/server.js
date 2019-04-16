dc4e530877de7beb4ba7cd8b3db20e7e7a72de5b
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2019-04-16 15:20:57 -0300

    WebSocket config

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+	socket.on('connectRoom', box => {
+		socket.join(box)
+	})	
+});
 
 mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-kyv3l.mongodb.net/omnistack?retryWrites=true", 
 	{
@@ -10,10 +22,16 @@ mongoose.connect("mongodb+srv://omnistack:omnistack@cluster0-kyv3l.mongodb.net/o
 	}
 );
 
+app.use((req, res, next) => {
+	req.io = io;
+
+	return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
